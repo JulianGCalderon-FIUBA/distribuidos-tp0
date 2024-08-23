d705ff611f5ed599f1e15bb6a63f4b3606bb158a
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2024-08-23 00:09:34 -0300

    Add generar-compose.sh script

---
+++ generar-compose.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+
+if [ "$#" -ne 2 ]; then
+  echo "Generates a compose.yaml file with a dynamic number of clients"
+  echo
+  echo "Usage: $0 <output> <clients>"
+  exit 1
+fi
+OUTPUT=$1
+CLIENTS=$2
+
+# validate $CLIENTS is a number
+if ! [ "$CLIENTS" -eq "$CLIENTS" ] 2>/dev/null; then
+  echo "$CLIENTS is not a number"
+  exit 1
+fi
+
+append() {
+  echo "$1" >> "$OUTPUT"
+}
+
+# truncate file
+: > "$OUTPUT"
+
+append "name: tp0"
+append "services:"
+append "  server:"
+append "    container_name: server"
+append "    image: server:latest"
+append "    entrypoint: python3 /main.py"
+append "    environment:"
+append "      - PYTHONUNBUFFERED=1"
+append "      - LOGGING_LEVEL=DEBUG"
+append "    networks:"
+append "      - testing_net"
+append ""
+
+for i in $(seq 1 "$CLIENTS"); do
+append "  client$i:"
+append "    container_name: client$i"
+append "    image: client:latest"
+append "    entrypoint: /client"
+append "    environment:"
+append "      - CLI_ID=$i"
+append "      - CLI_LOG_LEVEL=DEBUG"
+append "    networks:"
+append "      - testing_net"
+append "    depends_on:"
+append "      - server"
+append ""
+done
+
+append "networks:"
+append "  testing_net:"
+append "    ipam:"
+append "      driver: default"
+append "      config:"
+append "        - subnet: 172.25.125.0/24"
