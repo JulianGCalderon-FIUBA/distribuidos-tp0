eea4d6a4e55e8257f83d3fea2a7a28e6ce6dccec
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2024-09-02 22:19:17 -0300

    Add close doc

--- common/closer.go
+++ common/closer.go
@@ -8,6 +8,10 @@ type Closer struct {
 	ch chan error
 }
 
+// Spawns a closer for the given resource. The closer funtion will be called
+// - When the context finalizes
+// - When the `Close` method is called
+// Guarantees that the close method is called only once.
 func SpawnCloser[Resource any](ctx context.Context, resource Resource, resourceCloser func(Resource) error) Closer {
 	ch := make(chan error)
 	go func() {
@@ -24,6 +28,8 @@ func SpawnCloser[Resource any](ctx context.Context, resource Resource, resourceC
 	return Closer{ch}
 }
 
+// Notifies the Closer to close it's associated resource, if it hasn't closed it yet.
+// Returns the error, if any, of executing the closer function.
 func (c Closer) Close() error {
 	c.ch <- nil
 	return <-c.ch
